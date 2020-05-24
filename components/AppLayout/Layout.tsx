c1831367b3d70a6458742e3a3692b007f355c910
Orlyanskaya Anna <annorl.em@gmail.com>
2020-05-24 15:39:45 +0300

    Создание проекта

---
+++ components/AppLayout/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Head from 'next/head';
+
+const withAppLayout = () => (ComposedComponent) => {
+	class AppLayout extends Component {
+		render() {
+			return (
+				<div>
+					<Head>
+						<title>Подбор персонала</title>
+						<link rel="stylesheet" href="/index.css" />
+						<link rel="icon" href="/icon.ico" />
+						<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+					</Head>
+
+					<ComposedComponent {...this.props} />
+				</div>
+			);
+		}
+	}
+
+	return AppLayout;
+};
+
+export default withAppLayout;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import withAppLayout from '../components/AppLayout/Layout'
+
+
+const Home = () => (
+    <>
+       уууууу
+    </>
+)
+
+export default withAppLayout()(Home)
